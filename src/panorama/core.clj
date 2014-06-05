c04a3a0930cbd4208314b2e1d3b9ad01d968e137
Gunnar Helgason <gunnar.helgason@gmail.com>
2014-06-05 22:10:03 +0200

    performance improvements

--- src/panorama/core.clj
+++ src/panorama/core.clj
@@ -1,62 +1,51 @@
 (ns panorama.core
-  (:require [clojure.java.io :refer [writer]]
+  (:require [clojure.java.io :refer [file output-stream]]
             [clojure.tools.cli :refer [parse-opts]])
   (:import (java.security MessageDigest))
   (:gen-class))
 
 (set! *warn-on-reflection* true)
+(set! *unchecked-math* true)
 
 (defn calculate-luhn
-  [pan]
-  (let [arr (int-array (map #(Character/digit ^Character % 10) pan))
-        len (alength arr)]
+  [^bytes arr]
+  (let [len (alength arr)]
     (loop [i 1 acc 0]
       (if (> i len)
-        (rem (* acc 9) 10)
+        (unchecked-remainder-int (* acc 9) 10)
         (let [multiplier (if (= (mod i 2) 0) 1 2)
               product (let [product (* multiplier (aget arr (- len i)))]
                         (if (>= product 10) (- product 9) product))]
           (recur (+ i 1) (+ acc product)))))))
 
-(defn string->bytes
-  (^bytes
-   [^String string]
-   (.getBytes string "utf-8")))
-
-(defn bytes->hex
-  [bytes]
-  (apply str (map (partial format "%02x") bytes)))
-
 (defn sha1
-  [^String s]
-  (let [md (MessageDigest/getInstance "SHA-1")]
-    (.update md (string->bytes s))
-    (bytes->hex (.digest md))))
+  (^bytes
+   [^bytes pan]
+   (let [md (MessageDigest/getInstance "SHA-1")]
+     (.update md pan)
+     (.digest md))))
 
-(defn build-pan
-  [iin n]
-  (let [pan (str iin n)]
-    (str pan (calculate-luhn pan))))
+(defn insert
+  [^bytes arr n]
+  (let [nlen (alength arr)]
+    (aset-byte arr 15 0)
+    (loop [i (- nlen 2) d n]
+      (if (= d 0)
+        arr
+        (do
+          (aset-byte arr i (unchecked-remainder-int d 10))
+          (recur (- i 1) (unchecked-divide-int d 10)))))))
 
-(defn zero-pad
-  [iin n]
-  (let [nlen (count (str n))]
-    (str (apply str (repeat (- (- 15 (count iin)) nlen) "0")) n)))
-
-(defn num-range
+(defn make-lookup-entries
   [iin start stop]
-  (map #(zero-pad iin %) (range start stop)))
-
-(defn pan-range
-  [iin start stop]
-  (map #(build-pan iin %) (num-range iin start stop)))
-
-(defn generate-sha1
-  [iin start stop]
-  (let [output-file (str "./" iin "_" start "-"(dec stop))]
-    (with-open [w (writer output-file)]
-      (doseq [pan (pan-range iin start stop)]
-        (.write w (str pan ":" (sha1 pan) "\n"))))))
+  (let [panbytes (byte-array 16 (map #(Character/digit ^Character % 10) iin))
+        output-file (str "./" iin "_" start "-"(dec stop))]
+    (with-open [w (output-stream (file  output-file))]
+      (dotimes [i (- stop start)]
+        (insert panbytes (+ start i))
+        (aset-byte panbytes 15 (calculate-luhn panbytes))
+        (.write w (sha1 panbytes))
+        (.write w panbytes)))))
 
 (defn split-range [total-elems nthreads]
   (let [per-thread (int (/ total-elems nthreads))]
@@ -69,7 +58,7 @@
         range-max (Math/pow 10 (- 15 (count iin)))
         ranges (split-range range-max nthreads)
         workers (doall (map (fn [[start stop]]
-                              (future (generate-sha1 iin start stop))) ranges))]
+                              (future (make-lookup-entries iin start stop))) ranges))]
     (doall (map deref workers))))
 
 (def cli-options

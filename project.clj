820b2479f916721375c3347a7bea5b48d2091234
Gunnar Helgason <gunnar.helgason@gmail.com>
2018-02-04 10:37:54 +0100

    update dependencies

--- project.clj
+++ project.clj
@@ -3,9 +3,9 @@
   :url "https://github.com/gunnarhelgason/panic"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/tools.cli "0.3.1"]
-                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.clojure/tools.cli "0.3.5"]
+                 [org.clojure/core.async "0.4.474"]]
   :aot :all
   :main panic.core)
 
